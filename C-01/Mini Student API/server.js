29bdbc56572a55afd2051e10ec96a5f5568d29c7
gegamax <advbnkib30@gmail.com>
2026-06-14 09:21:39 +0530

    Refactor server.js to disable lab route
    Commented out lab route and adjusted console log.

--- C-01/Mini Student API/server.js
+++ C-01/Mini Student API/server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = require('./src/routes/student/student')
-const lab = require('./src/routes/lab/lab')
+// const lab = require('./src/routes/lab/lab')
 const app = express()
 const port = 3000
 
 app.use('/students', router)
-app.use('/lab', lab)
+// app.use('/lab', lab)
 
 app.get('/', (req, res) => {
    res.status(200).json({"message": "Student API running"})
 })
 
-app.listen(port, ()=>{console.log(`Example app listening on port ${port}`)})
+app.listen(port, ()=>{console.log(app listening on port ${port}`)})
